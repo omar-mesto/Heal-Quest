a3b285763d9cd09621b204a080588619d8594123
Omisto014 <omisto014@gmail.com>
2025-01-17 03:28:17 +0300

    create New File For SnackBar

---
+++ shared/Snackbar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">  
+import { defineEmits, defineProps } from 'vue';
+
+const props = defineProps({  
+  show: { type: Boolean, required: true },  
+  message: { type: String, required: true },  
+  color: { type: String, default: 'error' },  
+});  
+
+const emit = defineEmits(['closeSnackBar','showSnackBar']);  
+const close_snack_bar = () => {  
+  emit('closeSnackBar');  
+};  
+
+
+</script>  
+
+<template>  
+  <VSnackbar  
+    :model-value="props.show"
+    :color="props.color"  
+    @close="close_snack_bar"  
+  >  
+    {{ props.message }}  
+    <template #action="{ attrs }">  
+      <VBtn  
+        text  
+        v-bind="attrs"  
+        @click="close_snack_bar"  
+      >  
+        Close  
+      </VBtn>  
+    </template>  
+  </VSnackbar>  
+</template>
