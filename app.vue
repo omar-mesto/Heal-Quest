e440909792bd1ace47efe4d4fd98e5a46b3e0e42
Omisto014 <omisto014@gmail.com>
2025-01-17 03:28:30 +0300

    move SnackBar to New Component

--- app.vue
+++ app.vue
@@ -1,36 +1,13 @@
 <script lang="ts" setup>
-
-import { useAuthStore } from './store/auth/user';
-
-const userAuthStore = useAuthStore()
 useHead({
   title: 'Heal Quest',
 })
-
 </script>
 
 <template>
   <div>
     <NuxtLayout>
       <NuxtPage />
-      
-    <VSnackbar
-      v-model="userAuthStore.snackbar.show"
-      :color="userAuthStore.snackbar.color"
-      @close="userAuthStore.closeSnackbar"
-    >
-      {{ userAuthStore.snackbar.message }}
-      <template #actions>
-        <VBtn
-          color="white"
-          vartain="text"
-          @click="userAuthStore.closeSnackbar"
-        >
-          Close
-        </VBtn>
-      </template>
-    </VSnackbar>
     </NuxtLayout>
-
   </div>
 </template>
