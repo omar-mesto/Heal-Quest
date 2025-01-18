d5f6c46595abe7b91b6b2591df918af0dbd53d78
Omisto014 <omisto014@gmail.com>
2025-01-19 01:27:46 +0300

    add Validations to Validators Ffile

--- services/validators.ts
+++ services/validators.ts
@@ -1,7 +1,10 @@
-export default {
-  rules: {
-    phoneNumberRule: (value: string) => /^\+[0-9]{12}$/.test(value) || 'invalid phone number',
-    userNameRule:(value:string)=>/^[0-9 a-z]/.test(value)||'invalid User Name',
-    passwordRule:(value:string)=>/^[0-9 a-z]/.test(value)||'invalid password',
+export default {  
+  rules: {  
+    phoneNumberRule: (value: string) => /^\+[0-9]{12}$/.test(value) || 'Invalid phone number',  
+    userNameRule: (value: string) => /^[a-zA-Z0-9\s]+$/.test(value) || 'Invalid username',
+    passwordRule: (value: string) => /^(?=.*[A-Za-z]|\d)[A-Za-z\d]$/.test(value) || 'Invalid password',
+    fullNameRule: (value: string) => /^[a-zA-Z\s]+$/.test(value) || 'Invalid full name', 
+    genderRule: (value: string) => /^(male|female)$/i.test(value) || 'Invalid gender', 
+    birthdateRule: (value: string) => /^(0?[1-9]|1[0-2])\/(0?[1-9]|[12][0-9]|3[01])\/(19|20)\d{2}$/.test(value) || 'Invalid birthdate. Use M/D/YYYY format',
   },
 }
