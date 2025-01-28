6097f8e9f923d06a1f00547d6d1ccae9610aada4
Omisto014 <omisto014@gmail.com>
2025-01-28 04:13:59 +0300

    update Default Layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,14 @@
 <script setup>
-import Footer from '~~/shared/Footer.vue';
-import NavBar from '~~/shared/NavBar.vue';
+import Footer from '@/shared/Footer.vue'
+import NavBar from '@/shared/NavBar.vue'
+
 </script>
 
 <template>
   <VApp>
-    <NavBar />
-    <slot />  
+    <NavBar>
+      <slot />
+    </NavBar>
     <Footer />
   </VApp>
 </template>
