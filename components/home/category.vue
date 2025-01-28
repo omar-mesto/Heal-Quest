f578891426b1a012920983dd8e2d4eddbc42eed1
Omisto014 <omisto014@gmail.com>
2025-01-28 04:12:22 +0300

    use UseAsyncData to get Category

--- components/home/category.vue
+++ components/home/category.vue
@@ -1,47 +1,43 @@
-<script lang="ts" setup>  
-import { onMounted, ref } from 'vue';
-import type { CategoryModel } from '../../models/categoryModel';
-import { api } from '../../utils/api';
+<script lang="ts" setup>
+// import { useCategories } from '@/queries/categories';
 
-const isLoading = ref(true);   
-const data = ref<CategoryModel[]>([]);
+// const {data,status}=  useCategories();
 
-onMounted(async () => {  
-  isLoading.value = true;   
-  data.value = await api.get<CategoryModel[]>('getCategories');  
-  isLoading.value = false;  
-});  
-</script>  
+</script>
 
-<template>  
-  <div>  
-    <VRow class="py-5">  
-      <VCol  
+<template>
+  <div class="">
+    category page
+  </div>
+  <!-- {{ status }} -->
+  <!-- <div>
+    <VRow class="py-5">
+      <VCol
         v-for="index in 6"
-        :key="isLoading ? index : data.result[index]?.id"  
-        cols="6"  
-        md="2"  
-      >  
-        <template v-if="isLoading">  
-          <VSkeletonLoader  
-            class="window-item-skeleton"  
-            card  
-            height="100"  
-          />  
-        </template>  
-        <template v-else>  
-          <VCard  
-            class="mx-auto text-center h-100 cursor-pointer"  
-          >  
-            <VAvatar size="50">  
-              <VImg :src="data.result[index]?.icon" />  
-            </VAvatar>  
-            <VCardText>  
-              {{ data.result[index]?.name.en }}  
-            </VCardText>  
-          </VCard>  
-        </template>  
-      </VCol>  
+        :key="isLoading ? index : data.result[index]?.id"
+        cols="6"
+        md="2"
+      >
+        <template v-if="isLoading">
+          <VSkeletonLoader
+            class="window-item-skeleton"
+            card
+            height="100"
+          />
+        </template>
+        <template v-else>
+          <VCard
+            class="mx-auto text-center h-100 cursor-pointer"
+          >
+            <VAvatar size="50">
+              <VImg :src="data.result[index]?.icon" />
+            </VAvatar>
+            <VCardText>
+              {{ data.result[index]?.name.en }}
+            </VCardText>
+          </VCard>
+        </template>
+      </VCol>
     </VRow>
-  </div>  
+  </div> -->
 </template>
