70a43af25540035a5618919a19896bd21f17cffb
Omisto014 <omisto014@gmail.com>
2025-01-28 04:21:41 +0300

    fix Style Error in Navbar

--- shared/NavBar.vue
+++ shared/NavBar.vue
@@ -1,56 +1,41 @@
 <script setup lang="ts">
 const navItems = [
-  { title: 'Home', value: 'Home', icon: 'mdi-home', route: '/' },
-  { title: 'Chat', value: 'Chat', icon: 'mdi-chat', route: '/chat' },
-  { title: 'Plans', value: 'Plans', icon: 'mdi-calendar-check', route: '/plans' },
-  { title: 'Profile', value: 'Profile', icon: 'mdi-account', route: '/userProfile/account' },
-];
+  { icon: 'mdi-home', route: '/', title: 'Home', value: 'Home' },
+  { icon: 'mdi-chat', route: '/chat', title: 'Chat', value: 'Chat' },
+  { icon: 'mdi-calendar-check', route: '/plans', title: 'Plans', value: 'Plans' },
+  { icon: 'mdi-account', route: '/userProfile/account', title: 'Profile', value: 'Profile' },
+]
 </script>
 
 <template>
   <div>
-    <VAppBar
-      class="d-none d-sm-flex bg-primary"
-      app
-    >
-      <VToolbarTitle>Logo</VToolbarTitle>
-      <VSpacer />
-      <div class="d-flex justify-center w-50">
-        <VBtn 
-          v-for="item in navItems" 
-          :key="item.value" 
-          @click="$router.push(item.route)"
-        >
-          <span>{{ item.title }}</span>
+    <VLayout>
+      <VAppBar>
+        <VToolbarTitle>Logo</VToolbarTitle>
+        <VSpacer />
+        <div class="d-flex justify-center w-50">
+          <VBtn
+            v-for="item in navItems"
+            :key="item.value"
+            @click="$router.push(item.route)"
+          >
+            <span>{{ item.title }}</span>
+          </VBtn>
+        </div>
+        <VSpacer />
+        <VBtn icon>
+          <VIcon>mdi-heart</VIcon>
         </VBtn>
-      </div>
-      <VSpacer />
-      <VBtn icon>
-        <VIcon>mdi-heart</VIcon>
-      </VBtn>
-      <VBtn
-        icon
-        @click="$router.push('/userProfile')"
-      >
-        <VIcon>mdi-account</VIcon>
-      </VBtn>
-      <slot />
-    </VAppBar>
-    <VBottomNavigation
-      class="d-flex d-sm-none bg-primary"
-      app
-    >
-      <VBtn 
-        v-for="item in navItems" 
-        :key="item.value" 
-        @click="$router.push(item.route)"
-      >
-        <VIcon>{{ item.icon }}</VIcon>
-        <span>{{ item.title }}</span>
-      </VBtn>
-    </VBottomNavigation>
+        <VBtn
+          icon
+          @click="$router.push('/userProfile')"
+        >
+          <VIcon>mdi-account</VIcon>
+        </VBtn>
+      </VAppBar>
+      <VMain>
+        <slot />
+      </VMain>
+    </VLayout>
   </div>
 </template>
-
-<style scoped>
-</style>
