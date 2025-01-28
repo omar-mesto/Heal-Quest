ea9afd66dfa13244483347b495947df0ec7e8332
Omisto014 <omisto014@gmail.com>
2025-01-28 04:18:51 +0300

    update vue-i18n plugin

--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -1,15 +1,16 @@
 import { createI18n } from 'vue-i18n'
+
 import ar from '../locales/ar.json'
 import en from '../locales/en.json'
 
 export default defineNuxtPlugin(({ vueApp }) => {
   const i18n = createI18n({
-    legacy: false,
     globalInjection: true,
+    legacy: false,
     locale: 'en',
     messages: {
-      en, ar
-    }
+      ar, en,
+    },
   })
 
   vueApp.use(i18n)
