93fda04dbe51990051782b82ba282803e86337d9
Omisto014 <omisto014@gmail.com>
2025-01-28 04:04:11 +0300

    update Validation in Validators File

---
+++ utils/validators.ts
@@ -0,0 +1,10 @@
+export default {
+  rules: {
+    birthdateRule: (value: string) => /^(0?[1-9]|1[0-2])\/(0?[1-9]|[12][0-9]|3[01])\/(19|20)\d{2}$/.test(value) || 'Invalid birthdate. Use M/D/YYYY format',
+    fullNameRule: (value: string) => /^[a-zA-Z\s]+$/.test(value) || 'Invalid full name',
+    genderRule: (value: string) => /^(male|female)$/i.test(value) || 'Invalid gender',
+    passwordRule: (value: string) => /^(?=.*[A-Za-z]|\d)[A-Za-z\d]$/.test(value) || 'Invalid password',
+    phoneNumberRule: (value: string) => /^\+[0-9]{12}$/.test(value) || 'Invalid phone number',
+    userNameRule: (value: string) => /^[a-zA-Z0-9\s]+$/.test(value) || 'Invalid username',
+  },
+}
