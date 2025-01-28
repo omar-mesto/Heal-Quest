5940a74e87f68dfed953a019ca1689728d33eca8
Omisto014 <omisto014@gmail.com>
2025-01-28 04:13:15 +0300

    use UseAsyncData to Get Advertisement

--- components/home/Index.vue
+++ components/home/Index.vue
@@ -1,45 +1,39 @@
-<script lang="ts" setup>  
-import { onMounted, ref } from 'vue';
-import type { AdvertisementModel } from '../../models/advertismentModel';
-import { api } from '../../utils/api';
+<script lang="ts" setup>
+import { AdvertisementModel } from '@@/models/advertismentModel'
+import { useAPI } from '@@/services/baseApi'
+import { ref } from 'vue'
 
-const window = ref(false);  
-const data = ref<AdvertisementModel[]>([]);  
-const isLoading = ref(true);  
+const window = ref(false)
+const { data, status } = await useAPI<AdvertisementModel[]>('/getAdvertisment')
 
-onMounted(async () => {  
-  isLoading.value = true; 
-  data.value = await api.get<AdvertisementModel[]>('getAdvertisment');
-  isLoading.value = false;
-});  
-</script>  
+</script>
 
-<template>  
-  <div>  
-    <template v-if="isLoading">  
-      <VSkeletonLoader  
-        class="window-item-skeleton mt-sm-14 mt-0"  
-        image  
-        height="300"  
-      />  
-    </template>  
-    <template v-else>  
-      <VWindow  
-        v-model="window"  
-        class="mt-sm-14 mt-0"  
-        show-arrows  
-      >  
-        <VWindowItem  
-          v-for="advertisement in data.result"  
-          :key="advertisement.id"  
-        >  
-          <VImg  
-            :src="advertisement.image"  
-            cover  
-            height="300"  
-          />  
-        </VWindowItem>  
-      </VWindow>  
-    </template>  
-  </div>  
+<template>
+  <div>
+    <template v-if="status !='success'">
+      <VSkeletonLoader
+        class="window-item-skeleton mt-sm-14 mt-0"
+        image
+        height="300"
+      />
+    </template>
+    <template v-else>
+      <VWindow
+        v-model="window"
+        class="mt-sm-14 mt-0"
+        show-arrows
+      >
+        <VWindowItem
+          v-for="advertisement in data?.result"
+          :key="advertisement.id"
+        >
+          <VImg
+            :src="advertisement.image"
+            cover
+            height="300"
+          />
+        </VWindowItem>
+      </VWindow>
+    </template>
+  </div>
 </template>
