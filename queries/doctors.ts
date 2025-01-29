49dd5ab7ecf02006012e06aec5ab2395a549f06a
Omisto014 <omisto014@gmail.com>
2025-01-29 16:01:08 +0300

    use useAsyncData to Get Doctors

--- queries/doctors.ts
+++ queries/doctors.ts
@@ -1,3 +1,4 @@
+import { useAsyncData } from 'nuxt/app'
 import { getDoctors } from '../services/api'
 import { useAPI } from '../services/baseApi'
 
@@ -5,5 +6,5 @@ export const useDoctors = () => {
   return useAsyncData('doctors', () => getDoctors())
 }
 export const useCreateDoctor = (doctor) => {
-  return useAPI('/crateDoctor', { body: doctor, method: 'post' })
+  return useAPI('/crateDoctor',doctor,"doctors","POST")
 }
