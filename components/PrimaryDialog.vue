5d1213034e98300f0417da6491f28439cc966e33
Omisto014 <omisto014@gmail.com>
2025-01-30 22:11:33 +0300

    update Dialog Component

--- components/PrimaryDialog.vue
+++ components/PrimaryDialog.vue
@@ -1,41 +1,45 @@
 <script setup lang="ts">
-const model = defineModel<boolean>();
+const model = defineModel<boolean>()
 defineProps<{
-  title:string
-  icon?:string
+  icon?: string
+  title: string
 }>()
 defineEmits(['close'])
 </script>
+
 <template>
-
-<VDialog
-persistent
-                  v-model="model"
-                  max-width="40%"
-
-                >
-             <VContainer>
-
-                  <VCard>
-                    <VCardTitle class="d-flex align-center justify-space-between " >
-                      <div class="d-flex">
-
-                        <VIcon color="grey-darken-3" class="me-2">{{ icon ? icon : 'mdi-account-plus-outline' }}</VIcon>
-                        <p class="text-h5 text-grey-darken-3">{{ title }}</p>
-                      </div>
-                      <div>
-                        <VBtn @click="$emit('close')" variant="text" icon="mdi-close" size="large" />
-                      </div>
-
-                      
-                    </VCardTitle>
-<VCardText>
-  <slot></slot>
-</VCardText>
-           
-                
-                  </VCard>
-             </VContainer>    
-
-                </VDialog>
+  <VDialog
+    v-model="model"
+    persistent
+    max-width="40%"
+  >
+    <VContainer>
+      <VCard>
+        <VCardTitle class="d-flex align-center justify-space-between ">
+          <div class="d-flex">
+            <VIcon
+              color="grey-darken-3"
+              class="me-2"
+            >
+              {{ icon ? icon : 'mdi-account-plus-outline' }}
+            </VIcon>
+            <p class="text-h5 text-grey-darken-3">
+              {{ title }}
+            </p>
+          </div>
+          <div>
+            <VBtn
+              variant="text"
+              icon="mdi-close"
+              size="large"
+              @click="$emit('close')"
+            />
+          </div>
+        </VCardTitle>
+        <VCardText>
+          <slot />
+        </VCardText>
+      </VCard>
+    </VContainer>
+  </VDialog>
 </template>
