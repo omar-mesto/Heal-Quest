c3ea84f2352ff316dfce151e1de0fd21b3f34079
Omisto014 <omisto014@gmail.com>
2025-01-30 22:15:57 +0300

    add Pagination to getDoctors API

--- queries/doctors.ts
+++ queries/doctors.ts
@@ -1,10 +1,21 @@
-import { useAsyncData } from 'nuxt/app'
-import { getDoctors } from '../services/api'
-import { useAPI } from '../services/baseApi'
-
-export const useDoctors = () => {
-  return useAsyncData('doctors', () => getDoctors())
+import { useAsyncData } from 'nuxt/app';
+import { Ref } from "vue";
+import { api, useAPI } from '../services/baseApi';
+export const useDoctors = (params:{skip:Ref<number>,limit:Ref<number>}) => {
+  
+  return useAsyncData('doctors', () => api(`/getDoctor`, {
+    params: {
+      skip: params.skip.value,
+      limit:params.limit.value
+    } 
+  }), {
+    watch: [params.limit,params.skip]
+  }
+)
 }
 export const useCreateDoctor = (doctor) => {
-  return useAPI('/crateDoctor',doctor,"doctors","POST")
+  return useAPI({url:'/crateDoctor',payload:doctor,queryKey:'doctors',type:'POST'})
+}
+export const useDeleteDoctor = (doctorId:string) => {
+  return useAPI({ url: `/deleteDoctor`,params:{id:doctorId} ,queryKey: 'doctors' , type:'DELETE'})
 }
