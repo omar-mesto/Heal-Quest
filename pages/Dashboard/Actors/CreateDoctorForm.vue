6fe4231bd63b1753440b33c0c28de4dc26fecbd7
Omisto014 <omisto014@gmail.com>
2025-01-30 22:14:43 +0300

    update Create New Doctor Dialog

--- pages/Dashboard/Actors/CreateDoctorForm.vue
+++ pages/Dashboard/Actors/CreateDoctorForm.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { useCreateDoctor } from '@@/queries/doctors';
-import validators from '@@/utils/validators';
-import { computed, ref } from 'vue';
+import { useCreateDoctor } from '@@/queries/doctors'
+import validators from '@@/utils/validators'
+import { computed, ref } from 'vue'
 const emit = defineEmits(['close'])
 const doctorForm = ref({
   birthdate: null,
@@ -12,30 +12,13 @@ const doctorForm = ref({
   password: '',
   username: '',
 })
-const snackbar = ref({ color: 'error', message: '', show: false })
-
-
-const showSnackbar = (message: string, color = 'error') => {
-  snackbar.value.message = message
-  snackbar.value.color = color
-  snackbar.value.show = true
-}
-
 const createDoctorForm = ref()
 const createDoctor = async () => {
-  
-  const { error , status  } = await useCreateDoctor(doctorForm.value)
-  showSnackbar(error ?
-  error.value?.data?.error
-  : 'Doctor created successfully',
-  error ? 'error' : 'success',
+  const { status } = await useCreateDoctor(doctorForm.value)
 
-)
-if(status.value=='success')
+if (status.value == 'success')
 emit('close')
-
 }
-
 
 const addImageToDoctorForm = (event: Event) => {
   const file = (event.target as HTMLInputElement).files?.[0]
@@ -49,134 +32,130 @@ const addImageToDoctorForm = (event: Event) => {
 
   reader.readAsDataURL(file)
 }
-const isValidForm=computed(()=>createDoctorForm.value?.isValid)
+const isValidForm = computed(() => createDoctorForm.value?.isValid)
 </script>
+
 <template>
-<VContainer>
+  <VContainer>
     <VForm
-          ref="createDoctorForm"
-          validate-on="input"
-          @submit.prevent="createDoctor()"
+      ref="createDoctorForm"
+      validate-on="input"
+      @submit.prevent="createDoctor()"
+    >
+      <VCardText>
+        <VRow>
+          <VCol
+            cols="12"
+            md="6"
+            sm="6"
+          >
+            <VTextField
+              v-model="doctorForm.fullName"
+              :rules="[validators.rules.fullNameRule]"
+              label="Full name"
+            />
+          </VCol>
+          <VCol
+            cols="12"
+            md="6"
+            sm="6"
+          >
+            <VTextField
+              v-model="doctorForm.username"
+              :rules="[validators.rules.userNameRule]"
+              label="User name"
+            />
+          </VCol>
+          <VCol
+            cols="12"
+            md="6"
+            sm="6"
+          >
+            <VDateInput
+              v-model="doctorForm.birthdate"
+              label="Birth date"
+              append-inner-icon="mdi-calendar"
+              prepend-icon=""
+            />
+          </VCol>
+          <VCol
+            cols="12"
+            md="6"
+            sm="6"
+          >
+            <VTextField
+              v-model="doctorForm.mobileNumber"
+              :rules="[validators.rules.phoneNumberRule]"
+              label="Mobile number"
+            />
+          </VCol>
+          <VCol
+            cols="12"
+            md="6"
+            sm="6"
+          >
+            <VTextField
+              v-model="doctorForm.password"
+              :rules="[validators.rules.passwordRule]"
+              label="Password"
+              type="password"
+            />
+          </VCol>
+          <VCol
+            cols="12"
+            md="6"
+            sm="6"
+          >
+            <VFileInput
+              clearable
+              label="Profile image"
+              required
+              prepend-icon=""
+              append-inner-icon="mdi-cloud-upload-outline"
+              @change="addImageToDoctorForm"
+            />
+          </VCol>
+          <VCol
+            cols="12"
+            md="6"
+            sm="6"
+          >
+            <VRadioGroup
+
+              v-model="doctorForm.gender"
+              :rules="[validators.rules.genderRule]"
+              inline
+              color="primary"
+            >
+              <VRadio
+                label="Male"
+                value="male"
+              />
+              <VRadio
+                label="Female"
+                value="female"
+              />
+            </VRadioGroup>
+          </VCol>
+        </VRow>
+      </VCardText>
+      <VCardActions>
+        <VSpacer />
+        <VBtn
+          color="blue-darken-1"
+          @click="$emit('close')"
         >
-    <VCardText>
-
-
-          <VRow>
-            <VCol
-              cols="12"
-              md="6"
-              sm="6"
-            >
-              <VTextField
-                v-model="doctorForm.fullName"
-                :rules="[validators.rules.fullNameRule]"
-                label="Full name"
-              />
-            </VCol>
-            <VCol
-              cols="12"
-              md="6"
-              sm="6"
-            >
-              <VTextField
-                v-model="doctorForm.username"
-                :rules="[validators.rules.userNameRule]"
-                label="User name"
-              />
-            </VCol>
-            <VCol
-              cols="12"
-              md="6"
-              sm="6"
-            >
-              <VDateInput
-                v-model="doctorForm.birthdate"
-                label="Birth date"
-                append-inner-icon="mdi-calendar"
-                 prepend-icon=""
-              />
-            </VCol>
-            <VCol
-              cols="12"
-              md="6"
-              sm="6"
-            >
-              <VTextField
-                v-model="doctorForm.mobileNumber"
-                :rules="[validators.rules.phoneNumberRule]"
-                label="Mobile number"
-              />
-            </VCol>
-            <VCol
-              cols="12"
-              md="6"
-              sm="6"
-            >
-              <VTextField
-                v-model="doctorForm.password"
-                :rules="[validators.rules.passwordRule]"
-                label="Password"
-                type="password"
-              />
-            </VCol>
-            <VCol
-              cols="12"
-              md="6"
-              sm="6"
-            >
-              <VFileInput
-                clearable
-                label="Profile image"
-                required
-                prepend-icon=""
-                append-inner-icon="mdi-cloud-upload-outline"
-                @change="addImageToDoctorForm"
-              />
-            </VCol>
-            <VCol
-              cols="12"
-              md="6"
-              sm="6"
-            >
-              <VRadioGroup
-
-                v-model="doctorForm.gender"
-                :rules="[validators.rules.genderRule]"
-                inline
-                color="primary"
-              >
-                <VRadio
-                  label="Male"
-                  value="male"
-                />
-                <VRadio
-                  label="Female"
-                  value="female"
-                />
-              </VRadioGroup>
-            </VCol>
-          </VRow>
-        
-</VCardText>
-<VCardActions>
-                      <VSpacer />
-                      <VBtn
-                        color="blue-darken-1"
-                        @click="$emit('close')"
-                      >
-                        Cancel
-                      </VBtn>
-                      <VBtn
-                        color="blue-darken-1"
-                       variant="elevated"
-                       type="submit"
-                       :disabled="!isValidForm"
-                      >
-                        Save
-                      </VBtn>
-</VCardActions>
-</VForm>
-
-</VContainer>
-
+          Cancel
+        </VBtn>
+        <VBtn
+          color="blue-darken-1"
+          variant="elevated"
+          type="submit"
+          :disabled="!isValidForm"
+        >
+          Save
+        </VBtn>
+      </VCardActions>
+    </VForm>
+  </VContainer>
 </template>
