9b9db7db89b63a0a0cac30f2b4a888b63a4e2850
Omisto014 <omisto014@gmail.com>
2025-01-30 22:16:32 +0300

    update  BaseFile to Get Data

--- services/api.ts
+++ services/api.ts
@@ -1,10 +1,10 @@
-import { api } from './baseApi'
-
-export const getDoctors = async () => {
-  return api('/getDoctor')
+import { Ref } from "vue";
+import { api } from './baseApi';
+export const getDoctors = async (params:{skip:Ref<number>,limit:Ref<number>}) => {
+  return api(`/getDoctor`,{params:params})
 }
 export const getCategories = () => {
-return api('/getCategory')
+return api('/getCategories')
 }
 export const getAdvertisment = () => {
   return api('/getAdvertisment')
