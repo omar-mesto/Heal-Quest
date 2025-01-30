f024534b97bb8b2d5cb55084eb0279800fb36fc9
Omisto014 <omisto014@gmail.com>
2025-01-30 22:13:30 +0300

    add Container to Defaul Layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,17 @@
 <script setup>
-import Footer from '@/shared/Footer.vue';
-import NavBar from '@@/shared/NavBar.vue';
+import Footer from '@/shared/Footer.vue'
+import NavBar from '@@/shared/NavBar.vue'
 
 </script>
 
 <template>
+  <VContainer
+    fluid
+    class="pt-0"
+  >
     <NavBar>
       <slot />
     </NavBar>
     <Footer />
+  </VContainer>
 </template>
