9112c8cb9dc82f5fbf68df1712f4d21bb96c6085
Omisto014 <omisto014@gmail.com>
2025-02-03 00:34:40 +0300

    update Validators to Make User Able to Enter Arabic Name

--- utils/validators.ts
+++ utils/validators.ts
@@ -1,11 +1,11 @@
 export default {
   rules: {
     birthdateRule: (value: string) => /^(0?[1-9]|1[0-2])\/(0?[1-9]|[12][0-9]|3[01])\/(19|20)\d{2}$/.test(value) || 'Invalid birthdate. Use M/D/YYYY format',
-    fullNameRule: (value: string) => /^[a-zA-Z\s]+$/.test(value) || 'Invalid full name',
+    fullNameRule: (value: string) => /^[\u0600-\u06FFa-zA-Z\s]+$/.test(value) || 'Invalid full name',
     genderRule: (value: string) => /^(male|female)$/i.test(value) || 'Invalid gender',
-    passwordRule: (value: string) => /^(?=.*[A-Za-z]|\d)[A-Za-z\d]$/.test(value) || 'Invalid password',
+    passwordRule: (value: string) => /^(?=.*[A-Za-z]|\d)[A-Za-z\d]{1,}$/.test(value) || 'Invalid password',
     phoneNumberRule: (value: string) => /^\+[0-9]{12}$/.test(value) || 'Invalid phone number',
-    userNameRule: (value: string) => /^[a-zA-Z0-9\s]+$/.test(value) || 'Invalid username',
+    userNameRule: (value: string) => /^[\u0600-\u06FFa-zA-Z0-9\s]+$/.test(value) || 'Invalid username',
     imageRule: (value:string) => {if (!value) return 'Image is required.';},
   },
 }
