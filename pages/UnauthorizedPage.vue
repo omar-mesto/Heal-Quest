1eb4f8856ac77861b65e892446556ed8a0e5ba20
Omisto014 <omisto014@gmail.com>
2025-02-05 17:30:10 +0300

    udpate Middleware for Admin

--- pages/UnauthorizedPage.vue
+++ pages/UnauthorizedPage.vue
@@ -1,14 +1,19 @@
-
+<script lang="ts" setup>
+const onClickAction = async () => {
+  return  navigateTo({path:'/'}) 
+}
+</script>
 <template>
 <div>
   <VContainer class="fill-height">
     <VRow>
       <VCol>
-
         <v-empty-state
             headline="Whoops, 404"
             title="Page not found"
             text="The page you were looking for does not exist"
+            action-text="Go Back Home"
+            @click:action="onClickAction"
         ></v-empty-state>
       </VCol>
     </VRow>
